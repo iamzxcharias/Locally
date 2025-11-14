eff92a06246f1ec561263cb42843d0f0b7670a37
iamkn1ght <contact@zachpriller.com>
2025-11-14 15:02:04 +0100

    #1

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,5 +1,6 @@
 public class TestClass {
-    static void main() {
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
+
