b2bf062e3082ada94b5bc0748729eb755fc11a41
iamzxcharias <contact@zachpriller.com>
2026-01-19 15:23:30 +0100

    Added Not Blank Validation in EventRequest

--- src/main/java/api/dto/EventRequest.java
+++ src/main/java/api/dto/EventRequest.java
@@ -2,8 +2,10 @@ package api.dto;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
+import jakarta.validation.constraints.NotBlank;
 
 public class EventRequest {
+    @NotBlank(message = "Title must not be empty")
     public String title;
     public String category;
     public String description;
