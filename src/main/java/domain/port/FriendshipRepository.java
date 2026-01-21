6f3b833c6eba386720c963a776344abc237c417b
iamzxcharias <contact@zachpriller.com>
2026-01-21 20:25:29 +0100

    CleanUp FriendshipRepository

--- src/main/java/domain/port/FriendshipRepository.java
+++ src/main/java/domain/port/FriendshipRepository.java
@@ -1,18 +1,17 @@
 package domain.port;
 
-import domain.model.Friendship;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-import domain.model.FriendshipStatus;
+import domain.model.*;
+import java.util.*;
 
 public interface FriendshipRepository {
     void save(Friendship friendship);
+
     Optional<Friendship> findById(UUID id);
+    List<Friendship> findByUserId(UUID userId);
 
     boolean existsById(UUID id);
+
     void delete(UUID id);
-    List<Friendship> findByUserId(UUID userId);
 
     boolean existsByRequesterAndAddressee(UUID requesterId, UUID addresseeId);
     
