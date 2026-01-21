c3a7a6644f61d8f75b2bb262e6f60f2c4ca04073
iamzxcharias <contact@zachpriller.com>
2026-01-21 20:26:54 +0100

    CleanUp UserRepository

--- src/main/java/domain/port/UserRepository.java
+++ src/main/java/domain/port/UserRepository.java
@@ -1,14 +1,14 @@
 package domain.port;
 
 import domain.model.User;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 public interface UserRepository {
 
     void save(User user);
+
     boolean existsByEmail(String email);
+
     Optional<User> findById(UUID id);
     List<User> findAll();
 
