6c96a97b37bc1880dc649378a3d1245dc2ee760b
iamzxcharias <contact@zachpriller.com>
2026-01-21 20:26:20 +0100

    CleanUp ParticipationRepository

--- src/main/java/domain/port/ParticipationRepository.java
+++ src/main/java/domain/port/ParticipationRepository.java
@@ -1,17 +1,19 @@
 package domain.port;
 
-import domain.model.Participation;
-import domain.model.ParticipationStatus;
-import java.util.Optional;
-import java.util.UUID;
-import java.util.List;
+import domain.model.*;
+import java.util.*;
 
 public interface ParticipationRepository {
+
     void save(Participation participation);
+
     Optional<Participation> findById(UUID id);
     Optional<Participation> findByUserIdAndEventId(UUID userId, UUID eventId); // Wichtig für UC3!
+
     void delete(UUID id);
+
     long countByEventId(UUID eventId);
+
     List<Participation> search(UUID userId, UUID eventId, ParticipationStatus status, int page, int size);
     long countSearch(UUID userId, UUID eventId, ParticipationStatus status);
 }
