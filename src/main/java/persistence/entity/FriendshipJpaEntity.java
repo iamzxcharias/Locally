e65d7663cabbdd5fa4b996935906828979418df5
iamzxcharias <contact@zachpriller.com>
2026-01-21 20:38:45 +0100

    CleanUp FriendshipJpaEntity

--- src/main/java/persistence/entity/FriendshipJpaEntity.java
+++ src/main/java/persistence/entity/FriendshipJpaEntity.java
@@ -1,8 +1,8 @@
 package persistence.entity;
 
 import domain.model.FriendshipStatus;
-
 import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -11,7 +11,7 @@ import java.util.UUID;
 public class FriendshipJpaEntity {
 
     @Id
-    @Column(name = "id", updatable = false, nullable = false)
+    @Column(nullable = false, updatable = false)
     private UUID id;
 
     @Column(name = "requester_id", nullable = false)
@@ -24,14 +24,12 @@ public class FriendshipJpaEntity {
     private LocalDateTime createdAt;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "status", nullable = false)
+    @Column(nullable = false)
     private FriendshipStatus status;
 
-    // Standard-Konstruktor
     protected FriendshipJpaEntity() {
     }
 
-    // Konstruktor
     public FriendshipJpaEntity(UUID id, UUID requesterId, UUID addresseeId, LocalDateTime createdAt, FriendshipStatus status) {
         this.id = id;
         this.requesterId = requesterId;
@@ -40,10 +38,18 @@ public class FriendshipJpaEntity {
         this.status = status;
     }
 
-    // Getter
     public UUID getId() { return id; }
+    public void setId(UUID id) { this.id = id; }
+
     public UUID getRequesterId() { return requesterId; }
+    public void setRequesterId(UUID requesterId) { this.requesterId = requesterId; }
+
     public UUID getAddresseeId() { return addresseeId; }
+    public void setAddresseeId(UUID addresseeId) { this.addresseeId = addresseeId; }
+
     public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+
     public FriendshipStatus getStatus() { return status; }
+    public void setStatus(FriendshipStatus status) { this.status = status; }
 }
